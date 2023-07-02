3619bc6858bfb2aa3f3c459550a2951bde8b185d
Shokhrukh <shoxruxyarashovuz@gmail.com>
2023-07-02 22:36:34 +0500

    some_changes

--- lib/Pages/HomePage/Pages/Account.dart
+++ lib/Pages/HomePage/Pages/Account.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:water_tank_mobile/Data/Storage/LocalStorage.dart';
+import 'package:water_tank_mobile/Pages/SignInPage/SignInPage.dart';
 
 class Account extends StatelessWidget {
   const Account({Key? key}) : super(key: key);
@@ -15,7 +16,7 @@ class Account extends StatelessWidget {
 
         onPressed: (){
          LocalStorage.removeToken();
-
+         Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (c)=>const SignInPage()), (route) => false);
         }, child: const Row(
       mainAxisSize: MainAxisSize.min,
           children: [
